27fc1b5c46e842579bcf469746a1a90b1d40c612
Mxnu07 <manuelmoralesdiaz0@gmail.com>
2024-01-12 19:11:02 -0400

    fix: debuggin return

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -7,8 +7,7 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
+    if (tree && func)
 	/* first recur on left child */
 	binary_tree_inorder(tree->left, func);
 	/* then print the data of node */
