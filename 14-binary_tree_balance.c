064274897de61ad8efa53d5cc2fc810ad8f1d406
MMD <manuelmoralesdiaz0@gmail.com>
2024-01-12 19:30:43 -0400

    fixing betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -31,7 +31,7 @@ size_t _binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        if (!tree)
-                return (0);
-        return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
+	if (!tree)
+		return (0);
+	return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
 }
